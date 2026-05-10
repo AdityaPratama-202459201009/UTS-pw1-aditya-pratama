87510faf694eec3bca76330c2e7cdcf11fde7041
AdityaPratama-202459201009 <sayaadityapratama15@gmail.com>
2026-05-10 07:06:26 +0700

    menambahkan halaman pembayaran

---
+++ bayar.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+session_destroy();
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pembayaran</title>
+</head>
+<body>
+
+<h1>Pembayaran Berhasil</h1>
+
+<a href="index.php">
+Kembali
+</a>
+
+</body>
+</html>
